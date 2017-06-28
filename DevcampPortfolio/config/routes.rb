6b9163195fd8671647229815878a3ec8ff17500b
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 14:35:17 +0000

    Implemented basic authentication

--- DevcampPortfolio/config/routes.rb
+++ DevcampPortfolio/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolio#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
@@ -6,7 +7,12 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end  
+  end
+  
 
 root to: 'pages#home'
 
